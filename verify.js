9bab9dd7aec950c2ec45e5334477239c8081b315
Dalwer Hossain <dalwer.official@gmail.com>
2021-02-11 11:10:02 +0600

    add everthing

--- verify.js
+++ verify.js
@@ -20,17 +20,31 @@ depositBtn.addEventListener("click", function () {
     // const currentDepositNumber = parseFloat(currentDeposit);
     // const totalDeposit =  depositNumber+currentDepositNumber;
     // document.getElementById("currentDeposit").innerText = totalDeposit;
-    updateText("currentDeposit", depositNumber);
+    if(depositNumber<0)
+    {
+        alert("Deposit Number can not be negative");
+    }
+    else {
+        updateText("currentDeposit", depositNumber);
     updateText("currentBalance", depositNumber);
     document.getElementById("depositAmount").value = "";
+    }
+    
 })
 // Withdraw button handler
 const withdrawBtn = document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click", function () {
     const withdrawNumber = getInputNumber("withdrawAmount");
+    if(withdrawNumber<0)
+    {
+        alert("You have not enter valid money");
+    }
+    else {
     updateText("currentWithdraw", withdrawNumber);
     updateText("currentBalance",-1*withdrawNumber);
     document.getElementById("withdrawAmount").value = "";
+        }
+    
 })
 
 
@@ -44,5 +58,12 @@ function updateText(id, depositNumber) {
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
     const totalAmount = depositNumber + currentNumber;
-    document.getElementById(id).innerText = totalAmount;
+    if(totalAmount<0)
+    {
+        alert("You have not enough money");
+    }
+    else{
+        document.getElementById(id).innerText = totalAmount;
+    }
+    
 }
